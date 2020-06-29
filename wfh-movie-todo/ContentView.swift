a9aa591d87c76df600a85285c81dd567a7c3939c
Arie May Wibowo <ariemayw@gmail.com>
2020-06-30 04:59:39 +0700

    Initial Commit

---
+++ wfh-movie-todo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  wfh-movie-todo
+//
+//  Created by Arie May Wibowo on 30/06/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wfh-movie-todo/wfh_movie_todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  wfh_movie_todoApp.swift
+//  wfh-movie-todo
+//
+//  Created by Arie May Wibowo on 30/06/20.
+//
+
+import SwiftUI
+
+@main
+struct wfh_movie_todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
